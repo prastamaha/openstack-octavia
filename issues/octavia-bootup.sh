be61d9197eb6dba543b00d813336147ff522574b
prastamaha <prastamaha@gmail.com>
2020-08-03 09:28:44 +0700

    update

--- issues/octavia-bootup.sh
+++ issues/octavia-bootup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-source /etc/kolla/admin-openrc.sh
+source /etc/kolla/octavia-openrc.sh
 source /home/$USER/kolla-install/bin/activate
-
-sudo ip link set dev octavia-hm0 address $(openstack port list | grep octavia-hm-port | cut -d '|' -f 4)
 
 sudo ifconfig octavia-hm0 up
 
+MAC=$(openstack port list | grep octavia-hm-port | cut -d '|' -f 4)
+sudo ip link set dev octavia-hm0 address $MAC
+
 sudo docker exec -it openvswitch_vswitchd sudo dhclient octavia-hm0 2>/dev/null
 sudo docker exec -it openvswitch_vswitchd sudo ip route del default via 172.16.0.1 dev octavia-hm0 2> /dev/null
+
+MAC=$(openstack port list | grep octavia-hm-port | cut -d '|' -f 4)
+sudo ip link set dev octavia-hm0 address $MAC
 
 echo 'octavia status up'
